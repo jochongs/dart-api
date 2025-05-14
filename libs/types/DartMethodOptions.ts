8c93b805e16c816aa9ff2d4d003e3cdf927a3c66
jochongs <joch2712@naver.com>
2025-05-14 15:32:45 +0900

    chore: Add warning code comments on raw field on DartMethodOptions

--- libs/types/DartMethodOptions.ts
+++ libs/types/DartMethodOptions.ts
@@ -6,11 +6,15 @@ export type DartMethodOptions<T extends boolean> = {
    * `true`로 설정하면 API 응답을 Typescript 타입에 맞게 가공합니다.
    * `false`로 설정하면 모든 응답을 API 응답 받은 string 타입 그대로 반환됩니다.
    *
+   * ! 주의: 아직 구현이 불안정합니다. 사용하지 마십시오.
+   *
    * ## [EN]
    * Option to process the API response to match the Typescript type.
    *
    * `true` processes the API response to match the Typescript type.
    * `false` returns all responses as the original string type received from the API.
+   *
+   * ! Warning: This feature is not yet fully implemented. Please do not use it.
    *
    * @default true
    */
